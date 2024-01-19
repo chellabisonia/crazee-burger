2360bc7d2253dce98896f4b35742ff7e3fc69456
Sonia CHELLABI <chellabisonia@gmail.com>
2024-01-19 14:14:52 +0100

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  //state
+  const [inputValue, setInputValue] = useState("");
+  //comportements
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
+  const handleChange = (event) => {
+    const valueAfterChange = event.target.value;
+    setInputValue(valueAfterChange);
+  };
+  //affichage
+
+  return (
+    <div>
+      <h1>Bienvenue chez nous!</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          value={inputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder="Entrez votre prénom..."
+          required
+        />
+        <button>Accéder à votre espace</button>
+      </form>
+    </div>
+  );
 }
