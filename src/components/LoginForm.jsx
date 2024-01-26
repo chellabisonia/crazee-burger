a16e97e22a76a43f2e57adaa759a5e4f171db7e6
Sonia CHELLABI <chellabisonia@gmail.com>
2024-01-26 17:31:25 +0100

    💄 F03: LoginPage > add layout

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export default function LoginForm() {
   //state
@@ -17,20 +18,29 @@ export default function LoginForm() {
   };
   //affichage
   return (
-    <form action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous!</h1>
-      <br />
-      <h2>Connectez-vous</h2>
-      <input
-        value={inputValue}
-        onChange={handleChange}
-        type="text"
-        placeholder="Entrez votre prénom "
-        required="required"
-      />
-      {/* <Link to="/order-page"> */}
-      <button>Accéder à mon espace</button>
-      {/* </Link> */}
-    </form>
+    <LoginFormStyled action="submit" onSubmit={handleSubmit}>
+      <div>
+        <h1>Bienvenue chez nous!</h1>
+        <br />
+        <h2>Connectez-vous</h2>
+      </div>
+      <div>
+        <input
+          value={inputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder="Entrez votre prénom "
+          required="required"
+        />
+
+        <br />
+        
+        <button>Accéder à mon espace</button>
+      </div>
+    </LoginFormStyled>
   );
 }
+
+const LoginFormStyled = styled.form`
+  background: green;
+`;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,23 @@
+import styled from "styled-components";
 import LoginForm from "../components/LoginForm";
+import Logo from "../components/reusable-ui/Logo";
 
 export default function LoginPage() {
   return (
     <>
-      <LoginForm />
+      <LoginPageStyled>
+       <Logo />
+        <LoginForm />
+      </LoginPageStyled>
     </>
   );
 }
+
+const LoginPageStyled = styled.div`
+background: red;
+height: 100vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`;
