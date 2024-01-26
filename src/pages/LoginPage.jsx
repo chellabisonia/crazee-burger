a3b541f39fa920f77af279d70f3add96377ee163
Sonia CHELLABI <chellabisonia@gmail.com>
2024-01-26 20:37:07 +0100

    💄 F03: Logo > add styling + fonts + remome the scroll bars

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -20,18 +20,18 @@ const LoginPageStyled = styled.div`
   justify-content: center;
   align-items: center;
 
-  ::before {
+  &::before {
     content: "";
     background: url("/images/burger&fries-background.jpg") rgba(0, 0, 0, 0.7);
     background-size: cover;
     background-position: center;
     background-blend-mode: darken;
+
     position: absolute;
     top: 0;
     left: 0;
     right: 0;
     bottom: 0;
     z-index: -1;
-    overflow: hidden;
   }
 `;
