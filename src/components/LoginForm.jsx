0b4e32971ad9186cd9936a3957d61c30471d39dc
Sonia CHELLABI <chellabisonia@gmail.com>
2024-01-27 13:04:38 +0100

    💄 F03:LOGIN > LoginForm > input > Add styling

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { BsPersonCircle } from "react-icons/bs";
 
 export default function LoginForm() {
   //state
@@ -24,7 +25,8 @@ export default function LoginForm() {
         <hr />
         <h2>Connectez-vous</h2>
       </div>
-      <div>
+      <div className="inputContainer">
+        <BsPersonCircle className="icon" />
         <input
           value={inputValue}
           onChange={handleChange}
@@ -32,15 +34,21 @@ export default function LoginForm() {
           placeholder="Entrez votre prénom "
           required="required"
         />
-        <br />
-        <button>Accéder à mon espace</button>
       </div>
+      <br />
+      <button>Accéder à mon espace</button>
     </LoginFormStyled>
   );
 }
 
 const LoginFormStyled = styled.form`
-  /* background: green; */
+  background: green;
+  max-width: 500px;
+  min-width: 400px;
+  padding: 2.5rem 2rem;
+  margin: 0px auto;
+  text-align: center;
+  border-radius: 5px;
   font-family: "Amatic SC", cursive;
   hr {
     border: 1px solid #f56a2c;
@@ -56,7 +64,36 @@ const LoginFormStyled = styled.form`
     color: white;
     font-size: 36px;
   }
+
+  .inputContainer {
+    display: flex;
+    /* border: 1px solid red; */
+    align-items: center;
+    background-color: #ffffff;
+    border-radius: 5px;
+    padding: 18px 24px 18px 24px;
+    margin-bottom: 18px;
+    border-radius: 5px;
+  }
+  .icon {
+    font-size: 15px;
+    color: gray;
+    margin-right: 8px;
+  }
+  input {
+    border: none;
+    color: #17161a;
+    font-size: 15px;
+    background: white;
+    width: 100%;
+  }
+  &::placeholder {
+    background: white;
+    color: lightgray;
+  }
   button {
     font-family: Arial, Helvetica, sans-serif;
+    width: 240px;
+    height: 35px;
   }
 `;
