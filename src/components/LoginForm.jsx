085aeaae346a42782f83530bbc1cfcbaec18b3cc
Sonia CHELLABI <chellabisonia@gmail.com>
2024-01-27 10:35:21 +0100

    💄 F03: LOGIN> LoginForm > Add styling

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -21,7 +21,7 @@ export default function LoginForm() {
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
       <div>
         <h1>Bienvenue chez nous!</h1>
-        <br />
+        <hr />
         <h2>Connectez-vous</h2>
       </div>
       <div>
@@ -32,6 +32,7 @@ export default function LoginForm() {
           placeholder="Entrez votre prénom "
           required="required"
         />
+        <br />
         <button>Accéder à mon espace</button>
       </div>
     </LoginFormStyled>
@@ -39,5 +40,23 @@ export default function LoginForm() {
 }
 
 const LoginFormStyled = styled.form`
-  background: green;
+  /* background: green; */
+  font-family: "Amatic SC", cursive;
+  hr {
+    border: 1px solid #f56a2c;
+    margin-bottom: 40px;
+  }
+  h1 {
+    color: white;
+    font-size: 48px;
+  }
+  h2 {
+    color: #8e8b8b;
+    margin: 20px 10px 10px;
+    color: white;
+    font-size: 36px;
+  }
+  button {
+    font-family: Arial, Helvetica, sans-serif;
+  }
 `;
