bfaf1c23d236e03671408587624dbd66ef489940
Sonia CHELLABI <chellabisonia@gmail.com>
2024-02-08 13:34:27 +0100

    ♻️ F04: ORDER > Navbar > create NavbarRightSide

--- src/components/pages/order/Navbar.jsx
+++ src/components/pages/order/Navbar.jsx
@@ -1,17 +1,13 @@
 import { Link, useParams } from "react-router-dom";
 import styled from "styled-components";
+import NavbarRightSide from "./NavbarRightSide";
 
 export default function Navbar() {
   const { username } = useParams();
   return (
     <NavbarStyled>
       <div className="left-side">Left</div>
-      <div className="right-side">
-        <h1>Bonjour {username}</h1>
-        <Link to="/">
-          <button>Déconnexion</button>
-        </Link>
-      </div>
+      <NavbarRightSide username={username} />
     </NavbarStyled>
   );
 }
@@ -21,11 +17,7 @@ const NavbarStyled = styled.nav`
   height: 10vh;
   display: flex;
   justify-content: space-between;
-  
-  .right-side {
-    font-size: 5px;
-    background: pink;
-  }
+
   .left-side {
     background: purple;
   }
---
+++ src/components/pages/order/NavbarRightSide.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export default function NavbarRightSide({username}) {
+  return (
+    <NavbarRightSideStyled>
+      <h1>Bonjour {username}</h1>
+      <Link to="/">
+        <button>Déconnexion</button>
+      </Link>
+    </NavbarRightSideStyled>
+  );
+}
+const NavbarRightSideStyled = styled.div`
+  font-size: 5px;
+  background: pink;
+`;
