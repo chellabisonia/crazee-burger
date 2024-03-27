a68cbdd44ddfeca735c310838400ff9a7cfd1c0d
Sonia CHELLABI <chellabisonia@gmail.com>
2024-03-27 17:00:21 +0100

    💄 F07: MAIN > Add layout with Basket

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -6,21 +6,42 @@ export default function Main() {
   return (
     <MainStyled>
       {/* <div className="basket">Basket</div> */}
-      <Menu />
+      <div className="menu-and-admin">
+        <Menu />
+        <div className="admin">Admin</div>
+      </div>
     </MainStyled>
   );
 }
 
 const MainStyled = styled.div`
+  border: 3px solid green;
   background: ${theme.colors.background_white};
-  flex: 1; // or I can also use this: height: calc(95vh - 10vh);
+  /* flex: 1; // or I can also use this: height: calc(95vh - 10vh); */
+  height:calc(95vh - 10vh);
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   display: grid;
-  overflow-y: scroll;
+  /* overflow-y: scroll; */
   //this code will be used for Basket
   /* grid-template-columns: 25% 1fr; */
   /* .basket {
     background: pink;
   } */
+  .menu-and-admin {
+    position: relative;
+    overflow-y:hidden ;
+    display: grid;
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    border-bottom-right-radius: ${theme.borderRadius.extraRound};
+    .admin {
+      background: red;
+      position: absolute;
+      height: 250px;
+      bottom: 0;
+      left: 0;
+      right: 0;
+   
+    }
+  }
 `;
