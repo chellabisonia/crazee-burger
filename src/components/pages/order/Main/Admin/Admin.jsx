95b2ab76b0da769227be0218b3e72894be447f04
Sonia CHELLABI <chellabisonia@gmail.com>
2024-03-28 14:18:25 +0100

    F07: ADMIN > Add AdminTabs and AdminPanel

--- src/components/pages/order/Main/Admin/Admin.jsx
+++ src/components/pages/order/Main/Admin/Admin.jsx
@@ -1,26 +1,28 @@
 import styled from 'styled-components'
 import {theme} from "../../../../../theme/index";
+import AdminTabs from './AdminTabs';
+import AdminPanel from './AdminPanel';
 
 
 export default function Admin() {
   return (
-    <AdminStyled>Admin</AdminStyled>
+    <AdminStyled>
+      <AdminTabs/>
+      <AdminPanel/>
+    </AdminStyled>
    
   )
 }
 
 
-const AdminStyled = styled.div`background: red;
+const AdminStyled = styled.div`
 position: absolute;
-height: 250px;
 bottom: 0;
 left: 0;
 right: 0;
 
 
-background: ${theme.colors.white};
-border: 1px solid ${theme.colors.greyLight};
-box-shadow:${theme.shadows.subtle};
+
 
 `
 
