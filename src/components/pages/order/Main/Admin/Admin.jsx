bbbb257202883054106eedfbb834c7cc59662b7b
Sonia CHELLABI <chellabisonia@gmail.com>
2024-04-01 13:39:48 +0200

    🔥 F07: REMOVE AdminContext AND LIFT the state UP

--- src/components/pages/order/Main/Admin/Admin.jsx
+++ src/components/pages/order/Main/Admin/Admin.jsx
@@ -1,39 +1,23 @@
-import styled from 'styled-components'
-import {theme} from "../../../../../theme/index";
-import AdminTabs from './AdminTabs';
-import AdminPanel from './AdminPanel';
-import { useState } from 'react';
-import AdminContext from '../../../../../context/AdminContext';
-
+import styled from "styled-components";
+import { theme } from "../../../../../theme/index";
+import AdminTabs from "./AdminTabs";
+import AdminPanel from "./AdminPanel";
+import { useState } from "react";
 
 export default function Admin() {
-  //STATE
-  const [isCollapsed, setIsCollapsed] = useState(false);
-  
-  const adminContextValue = {isCollapsed, setIsCollapsed};
+  const[isCollapsed, setIsCollapsed] = useState(false);
+
   return (
- 
-  <AdminContext.Provider value={adminContextValue}>
-      <AdminStyled>
-       { !isCollapsed ? <AdminTabs/>
-        :  <AdminTabs/>
-  }
-   { isCollapsed && <AdminPanel/>
-  }
-      </AdminStyled>
-    </AdminContext.Provider>
-   
-  )
+    <AdminStyled>
+      <AdminTabs isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed}/>
+    {!isCollapsed && <AdminPanel />} 
+    </AdminStyled>
+  );
 }
 
-
 const AdminStyled = styled.div`
-position: absolute;
-bottom: 0;
-left: 0;
-right: 0;
-`
-
-    
- 
-  
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -1,29 +1,25 @@
 import styled from "styled-components";
 import Tab from "../../../../reusable-ui/Tab";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
-import { useContext } from "react";
-import AdminContext from "../../../../../context/AdminContext.jsx";
 import { theme } from "../../../../../theme/index.jsx";
 import { AiOutlinePlus } from "react-icons/ai";
 
-export default function AdminTabs() {
-  const { isCollapsed, setIsCollapsed } = useContext(AdminContext);
-
+export default function AdminTabs({ isCollapsed, setIsCollapsed }) {
   return (
     <AdminTabsStyled>
       <Tab
-        Icon={!isCollapsed ? <FiChevronUp /> : <FiChevronDown />}
+        Icon={isCollapsed ? <FiChevronUp /> : <FiChevronDown />}
         onClick={() => setIsCollapsed(!isCollapsed)}
-        className={!isCollapsed ? "is-active" : ""}
+        className={isCollapsed ? "is-active" : ""}
       />
 
-      <Tab
+      {/* <Tab
         Icon={<AiOutlinePlus />
       }
         onClick={() => setIsCollapsed(!isCollapsed)}
         className={!isCollapsed ? "is-active" : ""}
         label="Ajouter un produit"
-      />
+      /> */}
     </AdminTabsStyled>
   );
 }
@@ -32,12 +28,12 @@ const AdminTabsStyled = styled.div`
   display: flex;
   padding: 0 22px;
 
-  .is-active {
+   .is-active {
     background: ${theme.colors.background_dark};
     color: ${theme.colors.background_white};
     border-color: ${theme.colors.background_dark};
   }
   button{
     margin-left: 1px;
-  }
+  } 
 `;
